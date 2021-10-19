419d8daef49e8b445349192f8a0916b8d3476fb7
Crowdbotics <team@crowdbotics.com>
2021-10-19 23:10:09 +0000

    Initial commit.

---
+++ backend/damp_cherry_31574/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for damp_cherry_31574 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'damp_cherry_31574.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "damp_cherry_31574",
+  url: "https://damp_cherry_31574.botics.co",
+  api: "https://damp_cherry_31574.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
